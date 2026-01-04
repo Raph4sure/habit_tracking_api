a518732141e37e2ac11c450ab9aa5d399e63af4e
ALABI GBENGA RAPHAEL <Raph4sure007@gmail.com>
2026-01-04 16:43:25 +0100

    feat: Add initial API routes for authentication, habits, and users, implement environment variable handling, and update Prettier configuration.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
 import express from 'express';
 
+import authRoutes from './routes/authRoutes.ts'
+import userRoutes from './routes/userRoutes.ts'
+import hahabitRoutes from './routes/habitRoutes.ts'
+
+
 const app = express()
 
-app.get('/habit', (req, res) => {
-  res.json({ message: 'Habit fetched' })
+// Health check endooint (direct on app)
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    timeStamp: new Date().toISOString(),
+    service: 'Habit Tracker API'
+  })
 })
 
+
+// Mounting Routes
+
+app.use('api/auth', authRoutes)
+app.use('/api/habits', hahabitRoutes)
+app.use('/api/users', userRoutes)
+
+
 export { app }
+export default app
