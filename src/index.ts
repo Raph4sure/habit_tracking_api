ae9277ba5a2675df3716f890d706349ce985e679
ALABI GBENGA RAPHAEL <Raph4sure007@gmail.com>
2026-03-10 22:00:33 +0100

    feat: set up Vercel configuration and API routing, add .vercelignore, and clean up index files

--- src/index.ts
+++ src/index.ts
@@ -22,16 +22,3 @@ app.listen(env.PORT, () => {
 
 
 
-// console.log(`The server is listening on Port: ${env.PORT}`);
-// console.log(`Database Location: ${env.DATABASE_URL}`);
-
-// // This proves Zod turned the string "3000" into a real number
-// console.log(`Port Type: ${typeof env.PORT}`);
-
-// "scripts": {
-//     "dev": "cross-env APP_STAGE=dev node --watch src/index.ts",
-//     "start": "cross-env APP_STAGE=production node src/index.ts",
-//     "test": "cross-env APP_STAGE=test vitest run",
-//     "test:watch": "cross-env APP_STAGE=test vitest",
-//     "test:coverage": "cross-env APP_STAGE=test vitest run --coverage"
-//   },
